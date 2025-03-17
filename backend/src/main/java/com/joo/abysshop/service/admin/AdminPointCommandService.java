bf8dfc12f5c48b6980ed965f7fd849a68d7eb834
bam <juhyun19981129@gmail.com>
2025-03-17 16:47:16 +0900

    adminPointCommandService 구조변경

--- backend/src/main/java/com/joo/abysshop/service/admin/AdminPointCommandService.java
+++ backend/src/main/java/com/joo/abysshop/service/admin/AdminPointCommandService.java
@@ -2,7 +2,9 @@ package com.joo.abysshop.service.admin;
 
 import com.joo.abysshop.dto.admin.request.ProvidePointRequest;
 import com.joo.abysshop.entity.user.User;
+import com.joo.abysshop.repository.user.UserRepository;
 import com.joo.abysshop.service.user.UserQueryService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class AdminPointCommandService {
 
     private final UserQueryService userQueryService;
+    private final UserRepository userRepository;
 
     @Transactional
     public void providePoints(ProvidePointRequest providePointRequest) {
-        User user = userQueryService.findById(providePointRequest.userId());
+        User user = userRepository.findById(providePointRequest.userId())
+            .orElseThrow(() -> new EntityNotFoundException("유저가 존재하지 않습니다."));
         user.updatePointBalance(providePointRequest.points());
     }
 }
--- backend/src/main/java/com/joo/abysshop/service/user/UserQueryService.java
+++ backend/src/main/java/com/joo/abysshop/service/user/UserQueryService.java
@@ -1,8 +1,6 @@
 package com.joo.abysshop.service.user;
 
-import com.joo.abysshop.entity.user.User;
 import com.joo.abysshop.repository.user.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +11,4 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserQueryService {
 
     private final UserRepository userRepository;
-
-    //TODO: 안정성을 위해 엔티티 반환에서 DTO 반환으로 변경
-    public User findById(Long userId) {
-        return userRepository.findById(userId)
-            .orElseThrow(() -> new EntityNotFoundException("회원이 존재하지 않습니다."));
-    }
 }
