666a25a5929d1fa63743a41a96e7dccb802b50d1
bam <juhyun19981129@gmail.com>
2025-03-18 17:20:14 +0900

    누락되었던 구버전 OrderService 삭제

--- backend/src/main/java/com/joo/abysshop/controller/order/OrderController.java
+++ backend/src/main/java/com/joo/abysshop/controller/order/OrderController.java
@@ -2,10 +2,7 @@ package com.joo.abysshop.controller.order;
 
 import com.joo.abysshop.dto.order.request.CreateOrderRequest;
 import com.joo.abysshop.service.order.OrderCommandService;
-import com.joo.abysshop.service.order.OrderService;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class OrderController {
 
-    private final OrderService orderService;
     private final OrderCommandService orderCommandService;
 
     @PostMapping("/create")
