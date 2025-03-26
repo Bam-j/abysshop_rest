ecedb8c480c72553a008714ce70194bd2352494c
bam <juhyun19981129@gmail.com>
2025-03-26 23:18:01 +0900

    불필요 구버전 dependency 삭제, 잘못된 패키지 import 변경, 미사용 enum핸들러 삭제

--- backend/src/main/java/com/joo/abysshop/repository/cart/CartItemRepository.java
+++ backend/src/main/java/com/joo/abysshop/repository/cart/CartItemRepository.java
@@ -5,9 +5,9 @@ import com.joo.abysshop.entity.cart.CartItem;
 import com.joo.abysshop.entity.product.Product;
 import java.util.List;
 import java.util.Optional;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
