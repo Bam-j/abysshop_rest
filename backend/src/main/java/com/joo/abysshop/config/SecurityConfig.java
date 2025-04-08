b44a357cdaddc666e392c21fbbf4e600ea7a1f4f
bam <juhyun19981129@gmail.com>
2025-04-08 20:53:21 +0900

    SecurityConfig에 Option 수정

--- backend/src/main/java/com/joo/abysshop/config/SecurityConfig.java
+++ backend/src/main/java/com/joo/abysshop/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.joo.abysshop.service.user.CustomUserDetailsService;
 import com.joo.abysshop.util.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,6 +40,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(
                 SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(
                     "/api/home/**",
                     "/api/auth/**",
