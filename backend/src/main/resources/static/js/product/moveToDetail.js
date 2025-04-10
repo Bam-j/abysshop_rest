4bfab51c965476a2a318f86291bb51a061ffb2ec
bam <juhyun19981129@gmail.com>
2025-04-10 21:08:09 +0900

    구버전 코드(js, jsp)와 리소스 삭제 & todo 추가

--- backend/src/main/resources/static/js/product/moveToDetail.js
+++
@@ -1,8 +0,0 @@
-const items = document.getElementsByClassName('item');
-
-Array.from(items).forEach(item => {
-  item.addEventListener('click', () => {
-    const productId =  parseInt(item.dataset.productId);
-    window.location.href = `/product/detail/` + productId;
-  });
-});
