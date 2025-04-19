2c5c534f650c05a00a846e41aa7df055c8fa00bc
bam <juhyun19981129@gmail.com>
2025-04-19 16:21:39 +0900

    Railway 배포 테스트를 위한 AbysshopApplication.java 수정

--- backend/src/main/java/com/joo/abysshop/AbysshopApplication.java
+++ backend/src/main/java/com/joo/abysshop/AbysshopApplication.java
@@ -2,11 +2,13 @@ package com.joo.abysshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = "com.joo.abysshop.repository")
+@EntityScan(basePackages = "com.joo.abysshop.entity")
 public class AbysshopApplication {
 
     public static void main(String[] args) {
