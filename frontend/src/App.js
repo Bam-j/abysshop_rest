31449707c1699ee57af1d43a35fe9b8d4d274577
bam <juhyun19981129@gmail.com>
2025-04-21 18:05:41 +0900

    프론트엔드 Dockerfile 수정5, BrowserRounter > HashRouter로 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import SignInPage from './pages/SignInPage';
