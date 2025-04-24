fa3afa6934bb21dcd09e04e867cd458f12627282
bam <juhyun19981129@gmail.com>
2025-04-24 14:57:44 +0900

    SpringBootApplication excludeName 사용

--- backend/src/main/java/com/joo/abysshop/AbysshopApplication.java
+++ backend/src/main/java/com/joo/abysshop/AbysshopApplication.java
@@ -2,11 +2,17 @@ package com.joo.abysshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(
+    exclude = {SecurityAutoConfiguration.class},
+    excludeName = {
+        "org.springframework.boot.autoconfigure.orm.jpa.SharedEntityManagerAutoConfiguration"
+    }
+)
+
 //@EnableJpaRepositories(basePackages = "com.joo.abysshop.repository")
 //@EntityScan(basePackages = "com.joo.abysshop.entity")
 public class AbysshopApplication {
