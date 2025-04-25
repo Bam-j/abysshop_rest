2718355242b2ef71899783b8bda762d5d88ea414
bam <juhyun19981129@gmail.com>
2025-04-25 17:31:24 +0900

    배포 테스트모드로 axiosInstance 수정

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
+//const localBaseUrl = process.env.REACT_APP_LOCAL_API_BASE_URL;
 const baseURL = process.env.REACT_APP_API_BASE_URL;
-const localBaseUrl = process.env.REACT_APP_LOCAL_API_BASE_URL;
-
 
 const api = axios.create({
-  baseURL: `${localBaseUrl}/api`
-  //baseURL: `${baseURL}/api`,
+  //baseURL: `${localBaseUrl}/api`
+  baseURL: `${baseURL}/api`,
 });
 
 export default api;
